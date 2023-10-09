30f26676251893bfdc9fbbe282a710c3d8dcf62c
BearCurb <bigstarcat520@gmail.com>
2023-10-09 08:50:08 +0800

    chore: a liiter change

--- src/vue-src/views/Comments.vue
+++ src/vue-src/views/Comments.vue
@@ -1,18 +1,19 @@
 <script setup lang="tsx">
 import { Ref, h, onMounted, reactive, ref } from 'vue';
-import { ElTable } from 'element-plus';
+import { ElMessageBox, ElTable } from 'element-plus';
 import Comment from '../../common/Comment';
 import Task from '../../common/Task';
 
-const tableRef: Ref = ref(null)
+const commentOutTableRef: Ref = ref(null)
+const commentInTableRef: Ref = ref(null)
 const inputOrderId: Ref = ref("")
 
-const tableData = reactive<Comment[]>([])
+const commnetData = reactive<Comment[]>([])
 // const tableSearchData = reactive<Comment[]>([])
 
 onMounted(async () => {
     const comment: Comment[] = await window.electronAPI.brushService("commentService", "getAllComment")
-    tableData.push(...comment)
+    commnetData.push(...comment)
 })
 
 const handleMerge = (mainComment: Array<any>, appendComment: Array<any>): Array<any> => {
@@ -23,7 +24,8 @@ const handleMerge = (mainComment: Array<any>, appendComment: Array<any>): Array<
         const append = appendComment[i]
         result.push({
             main: main,
-            append: append
+            append: append,
+            ok: ref(false)
         })
     }
     return result;
@@ -49,22 +51,44 @@ const getDynamicButton = (comment: Comment) => {
     } else {
         return "淘宝"
     }
-    return h("el-button", {
-        size: "small", plain: ""
-    })
-    // return <></>
 }
 
-const handleRowDblclick = (row: Comment) => {
-    tableRef.value.toggleRowExpansion(row)
+const rowExpansopn = (row: Comment) => {
+    for (let i = 0; i < commnetData.length; i++) {
+        const data = commnetData[i]
+        if (data.uuid === row.uuid) {
+            commentOutTableRef.value.toggleRowExpansion(row, true)
+        } else {
+            commentOutTableRef.value.toggleRowExpansion(data, false)
+        }
+    }
 }
 
 const testSearch = async () => {
     const orderId = inputOrderId.value
     const task: Task = await window.electronAPI.brushService("taskService", "getSingleTask", orderId)
-    if (task.orderId.includes(orderId)) {
-        const productName = (/\d{4}(?=\D*$)/).exec(task.productName)
-        console.log(productName)
+    if (task && task.orderId && task.orderId.includes(orderId)) {
+        const regResult: RegExpExecArray | null = (/\d{4}(?=\D*$)/).exec(task.productName)
+        console.log(regResult)
+        if (regResult) {
+            commnetData.forEach(data => {
+                const resultData = []
+                if (data.name.includes(regResult[0])) {
+                    resultData.push(data)
+                }
+            })
+            ElMessageBox.alert('结果', 'Title', {
+                confirmButtonText: 'OK',
+                message: h("el-buton", null),
+            })
+
+
+            // commentOutTableRef.value.setCurrentRow(data)
+            // commentOutTableRef.value.toggleRowExpansion(data)
+            // commentOutTableRef.value.setScrollTop(((i + 1) * 33.76) - 10);
+            // rowExpansopn(data)
+
+        }
     }
 }
 
@@ -72,21 +96,21 @@ const testSearch = async () => {
 <template>
     <div class="comment-container">
         <div class="comment-search">
-            <el-form :inline="true" :size="`small`">
-                <el-form-item label="开始时间">
+            <el-form :inline="true">
+                <el-form-item label="用户ID">
                     <el-input type="text" v-model="inputOrderId" placeholder="输入" />
                 </el-form-item>
                 <el-form-item>
-                    <el-button @click="testSearch" type="primary" size="small">搜索</el-button>
+                    <el-button @click="testSearch" type="primary" size="small">分配</el-button>
                 </el-form-item>
             </el-form>
         </div>
         <div class="comment-option">
-            <el-button type="warning" round>淘宝分配</el-button>
+            <!-- <el-button type="warning" round>淘宝分配</el-button>
             <el-button type="danger" round>拼多多分配</el-button>
-            <el-button type="primary" round>问大家分配</el-button>
+            <el-button type="primary" round>问大家分配</el-button> -->
         </div>
-        <el-table ref="tableRef" @row-dblclick="handleRowDblclick" :data="tableData" show-overflow-tooltip
+        <el-table ref="commentOutTableRef" @row-dblclick="rowExpansopn" :data="commnetData" show-overflow-tooltip
             highlight-current-row stripe size="small">
             <el-table-column align="center" type="expand">
                 <template #default="props">
@@ -94,13 +118,10 @@ const testSearch = async () => {
                         <!-- 评语表格 -->
                         <!-- 评语表格 -->
                         <!-- 评语表格 -->
-                        <el-table stripe border size="small" show-overflow-tooltip highlight-current-row
-                            :data="handleMerge(props.row.mainComment, props.row.appendComment)">
-                            <el-table-column fixed align="center" label="状态" width="80px">
-                                <template #default="_scope">
-                                    <!-- <el-button size="small" type="danger" plain>状态</el-button> -->
-                                </template>
-                            </el-table-column>
+                        <el-table ref="commentInTableRef" stripe border size="small" show-overflow-tooltip
+                            highlight-current-row :data="handleMerge(props.row.mainComment, props.row.appendComment)">
+                            <!-- <el-table-column align="center" type="index"  /> -->
+                            <el-table-column align="center" label="序号" type="index" width="50" />
                             <el-table-column align="center" label="主" prop="main" />
                             <el-table-column align="center" label="操作" width="90px">
                                 <template #default="_scope">
@@ -116,12 +137,17 @@ const testSearch = async () => {
 
                             <el-table-column align="center" label="对应客户ID" width="200px">
                                 <template #default="_scope">
-                                    <el-input />
+                                    <el-input size="small" />
                                 </template>
                             </el-table-column>
                             <el-table-column align="center" label="对应订单号" width="200px">
                                 <template #default="_scope">
-                                    <el-input />
+                                    <el-input size="small" />
+                                </template>
+                            </el-table-column>
+                            <el-table-column align="center" label="付款状态" width="80px">
+                                <template #default="_scope">
+                                    <el-switch disabled style="--el-switch-on-color: #13ce66;" v-model="_scope.row.ok" />
                                 </template>
                             </el-table-column>
                         </el-table>
